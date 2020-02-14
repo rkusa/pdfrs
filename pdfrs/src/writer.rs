ebcaeddcbd5cbdf2908cfc2cf735376e9727f46a
Markus Ast <m@rkusa.st>
2020-02-14 10:56:01 +0100

    document Writer<W> and DocWriter<W>

--- pdfrs/src/writer.rs
+++ pdfrs/src/writer.rs
@@ -5,19 +5,54 @@ use crate::idseq::IdSeq;
 use crate::stream::{Stream, StreamRef};
 use serde_pdf::Reference;
 
+/// A type that is always [`Write`](std::io::Write), but either contains a `DocWriter<W>` or a
+/// [`Stream`](stream::Stream).
 pub enum Writer<W: io::Write> {
     Doc(DocWriter<W>),
     Stream(Stream<W>),
     Null,
 }
 
+/// A type that keeps track of a PDF XREF table while forwarding writes to its wrapped writer.
+///
+/// It keeps track of how many bytes have already been written to correctly reference objects
+/// inside the document.
 pub struct DocWriter<W: io::Write> {
     w: W,
     len: usize,
     xref: HashMap<usize, usize>, // <object id, offset>
 }
 
+impl<W: io::Write> Writer<W> {
+    /// Converts the current variant into a `Writer::Stream`.
+    pub fn into_stream(self, id_seq: &mut IdSeq) -> Self {
+        match self {
+            Writer::Doc(w) => Writer::Stream(Stream::new(id_seq, w)),
+            s => s,
+        }
+    }
+
+    /// Ends the current `Stream<W>`, if it is currently one. Returns a reference to the ended
+    /// stream.
+    pub fn end_stream(&mut self) -> Result<Option<Reference<StreamRef>>, io::Error> {
+        let out = mem::replace(&mut *self, Writer::Null);
+        match out {
+            Writer::Doc(w) => {
+                *self = Writer::Doc(w);
+                Ok(None)
+            }
+            Writer::Stream(stream) => {
+                let stream_ref = stream.to_reference();
+                *self = Writer::Doc(stream.end()?);
+                Ok(Some(stream_ref))
+            }
+            Writer::Null => unreachable!(),
+        }
+    }
+}
+
 impl<W: io::Write> DocWriter<W> {
+    /// Constructs a new `DocWriter<W>` wrapping the given writer.
     pub fn new(w: W) -> Self {
         DocWriter {
             w,
@@ -26,14 +61,18 @@ impl<W: io::Write> DocWriter<W> {
         }
     }
 
+    /// The length in bytes of the already written PDF output.
     pub fn len(&self) -> usize {
         self.len
     }
 
+    /// Add an XREF entry for the current position of the PDF output and assign it to the provided
+    /// `id`.
     pub fn add_xref(&mut self, id: usize) {
         self.xref.insert(id, self.len);
     }
 
+    /// Writes the XREF table into into the wrapped writer of the `DocWriter<W>`.
     pub fn write_xref(&mut self) -> Result<(), io::Error> {
         writeln!(self.w, "xref")?;
 
@@ -72,21 +111,6 @@ impl<W: io::Write> DocWriter<W> {
     }
 }
 
-impl<W> io::Write for DocWriter<W>
-where
-    W: io::Write,
-{
-    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
-        let len = self.w.write(buf)?;
-        self.len += len;
-        Ok(len)
-    }
-
-    fn flush(&mut self) -> Result<(), io::Error> {
-        self.w.flush()
-    }
-}
-
 impl<W: io::Write> io::Write for Writer<W> {
     fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
         match self {
@@ -105,28 +129,18 @@ impl<W: io::Write> io::Write for Writer<W> {
     }
 }
 
-impl<W: io::Write> Writer<W> {
-    pub fn into_stream(self, id_seq: &mut IdSeq) -> Self {
-        match self {
-            Writer::Doc(w) => Writer::Stream(Stream::new(id_seq, w)),
-            s => s,
-        }
+impl<W> io::Write for DocWriter<W>
+where
+    W: io::Write,
+{
+    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        let len = self.w.write(buf)?;
+        self.len += len;
+        Ok(len)
     }
 
-    pub fn end_stream(&mut self) -> Result<Option<Reference<StreamRef>>, io::Error> {
-        let out = mem::replace(&mut *self, Writer::Null);
-        match out {
-            Writer::Doc(w) => {
-                *self = Writer::Doc(w);
-                Ok(None)
-            }
-            Writer::Stream(stream) => {
-                let stream_ref = stream.to_reference();
-                *self = Writer::Doc(stream.end()?);
-                Ok(Some(stream_ref))
-            }
-            Writer::Null => unreachable!(),
-        }
+    fn flush(&mut self) -> Result<(), io::Error> {
+        self.w.flush()
     }
 }
 
